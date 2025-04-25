3226f79b2893ca26995407740a0c064383917a53
ober <oayalag@unal.edu.co>
2025-04-25 15:14:45 -0500

    Lab 2

--- homework/word_count.py
+++ homework/word_count.py
@@ -6,6 +6,7 @@ import fileinput
 import glob
 import os.path
 import time
+import string
 from itertools import groupby
 
 
@@ -17,8 +18,28 @@ from itertools import groupby
 # original se llama text0.txt, el archivo generado se llamará text0_1.txt,
 # text0_2.txt, etc.
 #
+
+
 def copy_raw_files_to_input_folder(n):
     """Funcion copy_files"""
+
+
+    if os.path.exists("files/input"):
+        for file in glob.glob("files/input/*"):
+            os.remove(file)
+        os.rmdir("files/input")
+    os.makedirs("files/input")
+
+    for file in glob.glob("files/raw/*"):
+        for i in range(1, n + 1):
+            with open(file, "r", encoding="utf-8") as f:
+                with open(
+                    f"files/input/{os.path.basename(file).split('.')[0]}_{i}.txt",
+                    "w",
+                    encoding="utf-8",
+                ) as f2:
+                    f2.write(f.read())
+
 
 
 #
@@ -37,8 +58,13 @@ def copy_raw_files_to_input_folder(n):
 #   ]
 #
 def load_input(input_directory):
-    """Funcion load_input"""
-
+   """Funcion load_input"""
+   sequence = []
+   files = glob.glob(f"{input_directory}/*")
+   with fileinput.input(files=files) as f:
+       for line in f:
+           sequence.append((fileinput.filename(), line))
+   return sequence
 
 #
 # Escriba la función line_preprocessing que recibe una lista de tuplas de la
@@ -46,7 +72,12 @@ def load_input(input_directory):
 # realiza el preprocesamiento de las líneas de texto,
 #
 def line_preprocessing(sequence):
-    """Line Preprocessing"""
+   """Line Preprocessing"""
+   sequence = [
+       (key, value.translate(str.maketrans("", "", string.punctuation)).lower())
+       for key, value in sequence
+   ]
+   return sequence
 
 
 #
@@ -62,8 +93,8 @@ def line_preprocessing(sequence):
 #   ]
 #
 def mapper(sequence):
-    """Mapper"""
-
+   """Mapper"""
+   return [(word, 1) for _, value in sequence for word in value.split()]
 
 #
 # Escriba la función shuffle_and_sort que recibe la lista de tuplas entregada
@@ -78,6 +109,9 @@ def mapper(sequence):
 #
 def shuffle_and_sort(sequence):
     """Shuffle and Sort"""
+    return sorted(sequence, key=lambda x: x[0])
+    
+
 
 
 #
@@ -88,6 +122,12 @@ def shuffle_and_sort(sequence):
 #
 def reducer(sequence):
     """Reducer"""
+ 
+    result = []
+    for key, group in groupby(sequence, lambda x: x[0]):
+        result.append((key, sum(value for _, value in group)))
+    return result
+
 
 
 #
@@ -96,6 +136,12 @@ def reducer(sequence):
 #
 def create_ouptput_directory(output_directory):
     """Create Output Directory"""
+
+    if os.path.exists(output_directory):
+        for file in glob.glob(f"{output_directory}/*"):
+            os.remove(file)
+        os.rmdir(output_directory)
+    os.makedirs(output_directory)
 
 
 #
@@ -108,6 +154,9 @@ def create_ouptput_directory(output_directory):
 #
 def save_output(output_directory, sequence):
     """Save Output"""
+    with open(f"{output_directory}/part-00000", "w", encoding="utf-8") as f:
+        for key, value in sequence:
+            f.write(f"{key}\t{value}\n")
 
 
 #
@@ -116,25 +165,36 @@ def save_output(output_directory, sequence):
 #
 def create_marker(output_directory):
     """Create Marker"""
+    with open(f"{output_directory}/_SUCCESS", "w", encoding="utf-8") as f:
+        f.write("")
+    
 
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
+
+
 def run_job(input_directory, output_directory):
-    """Job"""
-
-
+   """Job"""
+   sequence = load_input(input_directory)
+   sequence = line_preprocessing (sequence)
+   sequence = mapper (sequence)
+   sequence = shuffle_and_sort (sequence)
+   sequence = reducer (sequence)   
+   create_ouptput_directory (output_directory)
+   save_output (output_directory, sequence)
+   create_marker (output_directory)
+   
 if __name__ == "__main__":
+   
+   copy_raw_files_to_input_folder(n=1000)
 
-    copy_raw_files_to_input_folder(n=1000)
+   start_time = time.time()
 
-    start_time = time.time()
-
-    run_job(
-        "files/input",
-        "files/output",
-    )
-
-    end_time = time.time()
-    print(f"Tiempo de ejecución: {end_time - start_time:.2f} segundos")
+   run_job(
+       "files/input",
+       "files/output",
+   )
+   end_time = time.time()
+   print(f"Tiempo de ejecución: {end_time - start_time:.2f} segundos")
